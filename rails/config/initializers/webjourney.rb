4ad4fba7d10f6d0015b5d30aed25b9542f92d80b
Yohei Sasaki <yssk22@gmail.com>
2009-01-18 18:28:45 +0900

    add session expiration time

--- rails/config/initializers/webjourney.rb
+++ rails/config/initializers/webjourney.rb
@@ -1,2 +1,6 @@
+# Session Expiration
+ ActionController::Base.session_options[:session_expires] = Time.now + 1.month
+
+# logger for CouchDB Mapper class
 CouchResource::Base.logger = RAILS_DEFAULT_LOGGER
 
