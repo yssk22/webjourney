64099a327423b4ce1d9c0bd7dd2689aa07b32192
Yohei Sasaki <yssk22@gmail.com>
2009-02-11 23:08:25 +0900

    plugin directory refactored

---
+++ rails/vendor/plugins/webjourney/lib/package.rb
@@ -0,0 +1,3 @@
+WebJourney::Component::Task::Package.send :include, WebJourney::Component::Task::FileTasks
+WebJourney::Component::Task::Package.send :include, WebJourney::Component::Task::RegisterTasks
+WebJourney::Component::Task::Package.send :include, WebJourney::Component::Task::MigrationTasks
