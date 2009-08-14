2a5d7168cabc3ac0b5422dc357515cfdd0c1b90e
Yohei Sasaki <yssk22@macbook.local>
2009-08-14 16:28:56 +0900

    missing commit retried.

--- relax/apps/opensocial/views/people_ids_in_relationship/map.js
+++ relax/apps/opensocial/views/people_ids_in_relationship/map.js
@@ -1,5 +1,5 @@
 function(doc){
-  if(doc.type == "Group"){
+  if(doc.type == "Relationship"){
     for(var i in doc.tags){
       // key includes doc.to to be sorted.
       emit([doc.from, doc.tags[i], doc.to], null);
