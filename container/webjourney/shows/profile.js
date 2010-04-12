629cd5c3ac1c7f08c0d4e102f7995715964ab5c9
Yohei Sasaki <yssk22@gmail.com>
2010-04-12 23:18:05 +0900

    [mod] move profile page.

---
+++ container/webjourney/shows/profile.js
@@ -0,0 +1,43 @@
+function(doc, req){
+   // !code vendor/couchapp/path.js
+   var e = require('lib/errors');
+   var t = require('vendor/crayon/lib/template');
+   var ddoc = this;
+
+   // !code include/bindings.js
+   // log(bindings['page.stylesheets']);
+   if( doc ){
+      if( doc.type == "Person" ){
+         bindings["page.title"] = doc._id.split(":")[1] + "'s home page";
+         // set binding variables
+         bindings["p._id"] = doc._id;
+         bindings["p._rev"] = doc._rev;
+         bindings["p.displayName"] = doc.displayName;
+         if( doc.photo ){
+            bindings["p.photo"] = ["", req.info.db_name, doc._id, doc.photo].join("/");
+         }
+         var expected = "p:" + req.userCtx.name;
+         if( doc._id == expected ){
+            return t.render(ddoc.templates.site.html.header, bindings) +
+               t.render(ddoc.templates.pages.profile.my, bindings) +
+               t.render(ddoc.templates.site.html.footer, bindings);
+         }else{
+            return t.render(ddoc.templates.site.html.header, bindings) +
+               t.render(ddoc.templates.pages.profile.other, bindings) +
+               t.render(ddoc.templates.site.html.footer, bindings);
+         }
+      }
+   }else{
+      if( req.path.length == 6 ){
+         // id specified but not found.
+         var p_id = req.path[req.path.length - 1];
+         var expected = "p:" + req.userCtx.name;
+         if( p_id == expected ){
+            return t.render(ddoc.templates.site.html.header, bindings) +
+               t.render(ddoc.templates.pages.profile.create, bindings) +
+               t.render(ddoc.templates.site.html.footer, bindings);
+         }
+      }
+   }
+   return e.not_found(ddoc, bindings);
+}
